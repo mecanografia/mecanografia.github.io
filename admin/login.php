2d7e58714969422fe807067b17414bd2b284ba2b
Gregorio Álvaro Bueno Sabio <35528208+mecanografia@users.noreply.github.com>
2018-06-04 10:06:35 +0200

    Subida PHP
    prueba para probar si abre archivos php

---
+++ admin/login.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+include("../config.php");
+$email = $_POST['inputEmail'];
+$passwd = md5($_POST['inputPassword']);
+$consulta =mysqli_query ($mysqli, "SELECT email, password FROM usuario WHERE (email = '$email') AND (password = '$passwd')");
+$res = mysqli_num_rows($consulta);
+if ($res >= 1) {
+  $_SESSION['canAccess'] = true;
+  header('Location: dashboard.php');
+  }else {
+	  echo "Has metido la gamba pollo!!";
+  }
+?>
+<?php
+session_start();
+include("../config.php");
+
+$mailpost = $_POST['inputEmail'];
+$passwdpost = md5($_POST['inputPassword']);
+
+$consulta = mysqli_query ($mysqli, "SELECT email, password FROM usuario WHERE (email = '$mailpost') AND (password = '$passwdpost')");
+
+$cuenta = mysqli_num_rows($consulta);
+
+if($cuenta >= 1) {
+  $_SESSION['canAccess'] = true;
+  header('Location: dashboard.php');
+} else {
+  $_SESSION['canAccess'] = false;
+}
+mysqli_close($mysqli);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Alvaro Bueno">
+    <link rel="shortcut icon" href="../images/frozen-brain.png">
+    <title></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    <form class="form-signin" method="post">
+      <img class="mb-4" src="https://www.artescritorio.com/wp-content/uploads/2015/11/monsterball-portada.png" 
+      alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Catch in!! o logeate</h1>
+      <label for="inputUser" class="sr-only">Email Address</label>
+      <input type="text" name="inputEmail" class="form-control" placeholder="Email" autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" name="inputPassword" class="form-control" placeholder="Password">
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+</body>
+</html>
